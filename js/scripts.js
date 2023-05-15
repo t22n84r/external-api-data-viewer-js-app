da9968e240040ceff9265809a0baa906d09aa058
Taanvir Nazmee <81875062+t22n84r@users.noreply.github.com>
2023-05-15 17:49:17 -0400

    Achievement-2 Exercise-1.2
    https://careerfoundry.com/en/course/full-stack-immersion/exercise/javascript-basics-1#task
    
    Created an array and added details of 3 pokemon in it

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
-alert('Hello world');
+let pokemonList = [];
 
-let favoriteFood = "kurmaPulaw";
+let charizard = {name: 'charizard', height: '5 feet 7 inches', weight: '199.5 lbs', type: ['fire', 'flying']};
 
-function foodFunction() {
-    document.write(favoriteFood);
-  }
+pokemonList[0] = charizard;
+
+let venusaur = {name: 'venusaur', height: '6 feet 7 inches', weight: '220.5 lbs', type: ['grass', 'poison']};
+
+pokemonList[1] = venusaur;
+
+let blastoise = {name: 'blastoise', height: '5 feet 3 inches', weight: '188.5 lbs', type: 'water'};
+
+pokemonList[2] = blastoise;
+
+function pokemonFunction() {
+
+  document.getElementById("demo").innerHTML = JSON.stringify(pokemonList);
+}
+
+pokemonFunction()
